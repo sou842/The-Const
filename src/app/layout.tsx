8e6504065acfed36e39164e52fb4a45e7edd47d3
100x developer <developer@sharduls-MacBook-Pro.local>
2026-04-05 23:54:02 +0530

    add-google-analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Playfair_Display } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { Providers } from "@/components/Providers";
 
@@ -32,6 +33,19 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col font-sans">
         <Providers>{children}</Providers>
       </body>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-J5Z3XBSKTW"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-J5Z3XBSKTW');
+        `}
+      </Script>
     </html>
   );
 }
